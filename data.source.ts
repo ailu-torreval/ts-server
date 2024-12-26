34e4a93104af0400d457a0c31539a9ddb9e452af
ailu-torreval <ailu.torreval@gmail.com>
2024-12-26 16:59:33 +0100

    orders are automatically accepted

--- data.source.ts
+++ data.source.ts
@@ -6,21 +6,21 @@ dotenv.config();
 export const dbConfig: TypeOrmModuleOptions = {
     type: 'postgres',
     // FOR PRODUCTION
-    // url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL,
     // FOR DEVELOPMENT
-    host: process.env.DB_HOST,
-    port: +process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    // host: process.env.DB_HOST,
+    // port: +process.env.DB_PORT,
+    // username: process.env.DB_USERNAME,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_NAME,
     // END DEVELOPMENT
     synchronize: true,
     entities: [__dirname + '/../dist/**/*.entity{.ts,.js}'], // Adjusted path
     migrations: [__dirname + '/../dist/src/migrations/*{.ts,.js}'], // Adjusted path
     // FOR PRODUCTION
-    // ssl: {
-    //     rejectUnauthorized: false,
-    //   },
+    ssl: {
+        rejectUnauthorized: false,
+      },
 }
 
 const dataSource = new DataSource(dbConfig as DataSourceOptions);
